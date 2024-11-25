f9d561d7acecddbb1fa40bf152cdc03ac9567bc4
Haseung2 <dlgktmd0@naver.com>
2024-11-25 12:31:17 +0900

    [#17]fix :Timestamped deletedAt 추가
    - deletedAt 추가 및 application 에 `@EnableJpaAuditing` 추가

--- src/main/java/com/sparta/oceanbackend/OceanBackendApplication.java
+++ src/main/java/com/sparta/oceanbackend/OceanBackendApplication.java
@@ -2,8 +2,10 @@ package com.sparta.oceanbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OceanBackendApplication {
 
     public static void main(String[] args) {
