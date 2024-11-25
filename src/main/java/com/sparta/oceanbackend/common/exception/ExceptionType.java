5863c08e33454cc4ac921f5da5da84b9abf4c94b
Haseung2 <dlgktmd0@naver.com>
2024-11-25 11:36:37 +0900

    [#17]feat : 게시글 작성 기능 구현
    - 게시글 작성 기능 구현 및 글로벌 핸들러에 Validation exception 추가

--- src/main/java/com/sparta/oceanbackend/common/exception/ExceptionType.java
+++ src/main/java/com/sparta/oceanbackend/common/exception/ExceptionType.java
@@ -17,7 +17,8 @@ public enum ExceptionType {
     NAME_IN_USE("C001", "이미 사용 중인 사용자명입니다.", HttpStatus.CONFLICT),
     PASSWORD_MISMATCH("C002", "비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
     NAME_MISMATCH("C003", "사용자명이 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
-    USER_NOT_FOUND("C004", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    USER_NOT_FOUND("C004", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    IN_VALID_REQUEST("C005","잘못된 요청 값", HttpStatus.BAD_REQUEST);
 
     private final String errorCode;
     private final String message;
