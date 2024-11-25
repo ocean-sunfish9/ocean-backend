ef4f1098574b974f85503cb362e7de8cfbf9dc9b
CHOSEONGJUN <lionjojo@naver.com>
2024-11-25 14:14:07 +0900

    [#27]fix : 로그인유저 정보 받아오기 추가, 잘못된 카테고리 작성 예외처리 변경 - 기존의 로그인유저 정보 받아오던 방법변경, - `@ValidEnum` 을 통해 잘못된 카테고리 검증 추가

--- src/main/java/com/sparta/oceanbackend/api/post/dto/request/PostCreateRequest.java
+++ src/main/java/com/sparta/oceanbackend/api/post/dto/request/PostCreateRequest.java
@@ -1,7 +1,9 @@
 package com.sparta.oceanbackend.api.post.dto.request;
 
 import com.sparta.oceanbackend.api.enums.Categorys;
+import com.sparta.oceanbackend.common.annotation.ValidEnum;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
@@ -12,5 +14,7 @@ public class PostCreateRequest {
     private String title;
     @NotBlank(message = "내용을 입력해주세요")
     private String content;
-    private Categorys category;
+    @NotNull(message = "카테고리를 선택해주세요")
+    @ValidEnum(enumClass = Categorys.class, message = "올바른 카테고리를 선택해주세요")
+    private String category;
 }
