9667b5d5983bf9fc77d5f86d8e6b5d9fb98488c8
Haseung2 <dlgktmd0@naver.com>
2024-11-25 15:35:04 +0900

    [#19]feat : 게시글 수정 기능 구현
    - Post 클래스에 TimeStamped 클래스 상속 및 modifyPost 메서드 생성

--- src/main/java/com/sparta/oceanbackend/common/exception/ExceptionType.java
+++ src/main/java/com/sparta/oceanbackend/common/exception/ExceptionType.java
@@ -13,12 +13,14 @@ public enum ExceptionType {
      *
      * @since 2024-11-24
      */
-    // 공통기능(C)
+    // 공통기능(C) 게시판(P)
     NAME_IN_USE("C001", "이미 사용 중인 사용자명입니다.", HttpStatus.CONFLICT),
     PASSWORD_MISMATCH("C002", "비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
     NAME_MISMATCH("C003", "사용자명이 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
     USER_NOT_FOUND("C004", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    IN_VALID_REQUEST("C005","잘못된 요청 값", HttpStatus.BAD_REQUEST);
+    IN_VALID_REQUEST("C005","잘못된 요청 값", HttpStatus.BAD_REQUEST),
+    NON_EXISTENT_POST("P001", "존재하지 않는 게시물입니다.", HttpStatus.BAD_REQUEST),
+    NOT_WRITER_POST("P002","본인이 작성한 게시글이 아닙니다.", HttpStatus.BAD_REQUEST);
 
     private final String errorCode;
     private final String message;
--- src/main/java/com/sparta/oceanbackend/domain/post/entity/Post.java
+++ src/main/java/com/sparta/oceanbackend/domain/post/entity/Post.java
@@ -1,6 +1,7 @@
 package com.sparta.oceanbackend.domain.post.entity;
 
 import com.sparta.oceanbackend.api.enums.Categorys;
+import com.sparta.oceanbackend.common.entity.Timestamped;
 import com.sparta.oceanbackend.domain.comment.entity.Comment;
 import com.sparta.oceanbackend.domain.user.entity.User;
 import jakarta.persistence.CascadeType;
@@ -15,6 +16,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,7 +31,7 @@ import org.hibernate.annotations.DynamicInsert;
 @Getter
 @NoArgsConstructor
 @DynamicInsert // insert 시에 null 값 제외해서 count 값 지정해둔 default 값으로 들어가게끔
-public class Post {
+public class Post extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -52,5 +56,14 @@ public class Post {
         this.content = content;
         this.category = category;
         this.user = user;
+    }
+
+    public void modifyPost(
+        String title,
+        String content,
+        String category) {
+        this.title = title;
+        this.content = content;
+        this.category = Categorys.valueOf(category);
     }
 }
