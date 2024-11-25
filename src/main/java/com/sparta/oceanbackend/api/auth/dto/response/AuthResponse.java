23225dec869d304b1f960727f056fe878817bf53
Despereaux <157133321+Despereaux-MAU@users.noreply.github.com>
2024-11-25 12:22:02 +0900

    [#24] fix : `@RequiredArgsConstructor` 삭제

--- src/main/java/com/sparta/oceanbackend/api/auth/dto/response/AuthResponse.java
+++ src/main/java/com/sparta/oceanbackend/api/auth/dto/response/AuthResponse.java
@@ -2,10 +2,8 @@ package com.sparta.oceanbackend.api.auth.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
     private Long id;
