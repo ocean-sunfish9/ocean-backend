44db8589210954054f91f252cb422af06fc4f3e2
CHOSEONGJUN <lionjojo@naver.com>
2024-11-27 14:37:10 +0900

    [#46]fix : 게시글 단건조회시 조회수 상승 문제 - 게시글 조회시 조회수가 오르지 않던 문제 수정

--- src/main/java/com/sparta/oceanbackend/domain/post/entity/Post.java
+++ src/main/java/com/sparta/oceanbackend/domain/post/entity/Post.java
@@ -20,6 +20,7 @@ import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.Where;
@@ -31,38 +32,44 @@ import org.hibernate.annotations.Where;
 @DynamicInsert // insert 시에 null 값 제외해서 count 값 지정해둔 default 값으로 들어가게끔
 @Where(clause = "deleted_at IS NULL")
 public class Post extends Timestamped {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false, length = 50)
-    private String title;
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String content;
-    @Enumerated(EnumType.STRING)
-    private Categorys category;
-    @ColumnDefault("1") // default 값 1으로 지정
-    private Long count;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
+  @Column(nullable = false, length = 50)
+  private String title;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<Comment> comments;
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String content;
 
-    @Builder
-    public Post(String title, String content, Categorys category, User user) {
-        this.title = title;
-        this.content = content;
-        this.category = category;
-        this.user = user;
-    }
+  @Enumerated(EnumType.STRING)
+  private Categorys category;
 
-    public void modifyPost(
-        String title,
-        String content,
-        String category) {
-        this.title = title;
-        this.content = content;
-        this.category = Categorys.valueOf(category);
-    }
+  @ColumnDefault("1") // default 값 1으로 지정
+  @Setter
+  private Long count;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private User user;
+
+  @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<Comment> comments;
+
+  @Builder
+  public Post(String title, String content, Categorys category, User user) {
+    this.title = title;
+    this.content = content;
+    this.category = category;
+    this.user = user;
+  }
+
+  public void modifyPost(String title, String content, String category) {
+    this.title = title;
+    this.content = content;
+    this.category = Categorys.valueOf(category);
+  }
+
+  public void updateCount() {
+    this.count = this.count + 1;
+  }
 }
