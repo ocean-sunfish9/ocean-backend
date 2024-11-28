ad9f4539fb27e82d0c9fadecd76df7dbb7524524
Despereaux <157133321+Despereaux-MAU@users.noreply.github.com>
2024-11-28 20:14:58 +0900

    [#52]fix : 불필요한 테이블 및 클래스들 삭제

--- src/main/java/com/sparta/oceanbackend/domain/abuse/repository/IpAccessLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.oceanbackend.domain.abuse.repository;
-
-import com.sparta.oceanbackend.domain.abuse.entity.IpAccessLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IpAccessLogRepository extends JpaRepository<IpAccessLog, Long> {
-}
